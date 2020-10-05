a4a1998ffb4c03e9c3a95b211e98f7cf5d53f1d1
Zetta56 <67248032+Zetta56@users.noreply.github.com>
2020-10-05 12:05:49 -0400

    modify reshuffle diff logic

--- routes/card.js
+++ routes/card.js
@@ -3,7 +3,7 @@ const express = require("express"),
 	  middleware = require("../middleware"),
 	  Set = require("../models/Set"),
 	  Card = require("../models/Card");
-
+//note: move diff check and change random max
 router.post("/shuffle", async (req, res) => {
 	try {
 		const foundCards = await Card.find({set: req.params.setId});
@@ -11,23 +11,20 @@ router.post("/shuffle", async (req, res) => {
 			temp, 
 			diff = false;
 		while(!diff) {
-			//Swaps random card with last card (Fisher-Yates Shuffle)
 			for(i = foundCards.length - 1; i >= 0; i--) {
-				random = Math.floor(Math.random() * foundCards.length);
+				//Swaps random card with last card (Fisher-Yates Shuffle)
+				random = Math.floor(Math.random() * i);
 				temp = foundCards[random];
 				foundCards[random] = foundCards[i];
 				foundCards[i] = temp;
-			};
-			//Checks if randomized list is different from previous list
-			for(i = 0; i < foundCards.length; i++) {
-				if(req.body.length <= 1 || (req.body && req.body[0] && !foundCards[0]._id.equals(req.body[0]._id))) {
+				//Checks if randomized list is different from previous list
+				if(req.body.length <= 1 || (req.body && req.body[i] && !foundCards[i]._id.equals(req.body[i]._id))) {
 					diff = true;
 				};
 			};
 		};
 		res.json(foundCards);
 	} catch(err) {
-		console.log(err)
 		res.status(500).json(err);
 	};
 });
